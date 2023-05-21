ba6358d5d56a59765d133979036ccdb3ed7678e4
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-05-21 10:16:13 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+ res.send("hello world");
+})
+
+app.post('/home', function (req, res) {
+ res.send("Welcome to home");
+})
+
+app.patch('/home', function (req, res) {
+ // res.send("This is a patch request");
+ res.json({
+  message: "this is message",
+  success : true
+ })
+})
+
+app.listen(PORT, function process() {
+ console.log("Server Started");
+})
